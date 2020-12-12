b6e960204b951671645604c462b403779bdfbc8f
nkizito92 <nkizito57@gmail.com>
2020-12-12 10:02:54 -0600

    added a ClockContainer and a ClockIn component

---
+++ src/components/ClockIn.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class ClockIn extends Component {
+
+    state = {
+        id: 0,
+        error: "",
+        update: ""
+    }
+
+    render() {
+        return (
+            <div>
+                Hi there!!!
+            </div>
+        )
+    }
+}
+
+export default ClockIn
---
+++ src/container/ClockContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import ClockIn from '../components/ClockIn'
+class ClockContainer extends Component {
+    render() {
+        return (
+            <div>
+                <ClockIn />
+            </div>
+        )
+    }
+}
+
+export default ClockContainer
