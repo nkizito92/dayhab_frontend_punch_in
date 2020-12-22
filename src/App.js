237f985d445519c345e806015700d14dc645bf0e
nkizito92 <nkizito57@gmail.com>
2020-12-21 19:48:38 -0600

    added a route and included the store to fetchPunches

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import ClockIn from './components/ClockIn'
+import ClockContainer from './container/ClockContainer'
 import Navbar from './headers/Navbar'
+import { fetchPunches } from './action/clockAction'
+import { connect } from 'react-redux'
 import './css/style.css'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <nav>
-        <Navbar />
-      </nav>
-      <Switch>
-        <Route exact path="/" component={() => <ClockIn/>} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchPunches()
+  }
+ render () {
+
+   console.log(this.props.punches)
+   return (
+
+     <div className="App">
+     <nav>
+     <Navbar />
+     </nav>
+     <Switch>
+        <Route exact path="/" component={() => <ClockContainer punches={this.props.punches}/>} />
+        <Route exact path="/clients" component={() => <ClockContainer punches={this.props.punches}/>} />
+     </Switch>
+     </div>
+  ) 
+  }
+    
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+      punches: state.clockinsReducer.punches
+  }
+}
+
+export default connect(mapStateToProps, { fetchPunches })(App)
