7270d0c0ae3565b5e0003ffccd1859e0f96fd440
nkizito92 <nkizito57@gmail.com>
2020-12-27 09:58:47 -0600

    changed route component to clientsContainer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import ClockContainer from './container/ClockContainer'
+import ClientContainer from './container/ClientContainer'
 import Navbar from './headers/Navbar'
 import { fetchPunches } from './action/clockAction'
+import { fetchClients } from './action/clientAction'
 import { connect } from 'react-redux'
 import './css/style.css'
 import './App.css';
@@ -11,6 +13,7 @@ class App extends React.Component {
 
   componentDidMount() {
     this.props.fetchPunches()
+    this.props.fetchClients()
   }
  render () {
 
@@ -23,7 +26,7 @@ class App extends React.Component {
      </nav>
      <Switch>
         <Route exact path="/" component={() => <ClockContainer punches={this.props.punches}/>} />
-        <Route exact path="/clients" component={() => <ClockContainer punches={this.props.punches}/>} />
+        <Route exact path="/clients" component={() => <ClientContainer clients={this.props.clients} />} />
      </Switch>
      </div>
   ) 
@@ -33,8 +36,9 @@ class App extends React.Component {
 
 const mapStateToProps = state => {
   return {
-      punches: state.clockinsReducer.punches
+      punches: state.clockinsReducer.punches,
+      clients: state.clientsReducer.clients
   }
 }
 
-export default connect(mapStateToProps, { fetchPunches })(App)
+export default connect(mapStateToProps, { fetchPunches, fetchClients })(App)
