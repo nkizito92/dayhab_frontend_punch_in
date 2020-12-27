11a81d44489b68b766a105b4ab3f50eb8a654cac
nkizito92 <nkizito57@gmail.com>
2020-12-27 09:55:45 -0600

    added a clientAction

---
+++ src/action/clientAction.js
@@ -0,0 +1,18 @@
+import { onlineUrl } from './urlLink'
+export const addClient = client => {
+    return({
+        type: 'ADD_CLIENT',
+        action: client
+    })
+}
+
+export const fetchClients = () => {
+    return (dispatch) => {
+        dispatch({type: 'LOADING_CLIENTS'})
+        fetch(`${onlineUrl}/clients`)
+        .then(res => res.json())
+        .then(disClients => {
+            dispatch({type: 'ADD_CLIENTS', clients: disClients})
+        })
+    }
+}
