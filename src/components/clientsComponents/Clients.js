7d42be4a2068f6c45a8eda78f1f720947b79836a
nkizito92 <nkizito57@gmail.com>
2020-12-31 17:17:00 -0600

    adjusted some clients of code

--- src/components/clientsComponents/Clients.js
+++ src/components/clientsComponents/Clients.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Client from './Client'
-class Clients extends React.Component {
-    render () {
+class Clients extends Component {
+
+    display() {
+        return this.props.clients.map(client => {
+            return <Client key={client.id} client={client} />
+        })
+    }
+    render() {
         return (
-            this.props.clients.map(client => {
             <div>
-                <Client key={client.id} client={client} />
+                Hi
+                <div>
+                    <table border="1">
+                        <th>Clients</th>
+                        <tr><td>full_name</td><td>Pay Rate</td> <td>Total hours</td></tr>
+                        {this.display()}
+                    </table>
+                </div>
             </div>
-            })
         )
     }
 }
