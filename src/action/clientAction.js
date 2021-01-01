7966eb5303cfc4038487249654ed6c824e7284d7
nkizito92 <nkizito57@gmail.com>
2020-12-31 22:54:45 -0600

    createClient action has been added

--- src/action/clientAction.js
+++ src/action/clientAction.js
@@ -1,6 +1,6 @@
 import { onlineUrl } from './urlLink'
 export const addClient = client => {
-    return({
+    return ({
         type: 'ADD_CLIENT',
         action: client
     })
@@ -8,12 +8,26 @@ export const addClient = client => {
 
 export const fetchClients = () => {
     return (dispatch) => {
-        dispatch({type: 'LOADING_CLIENTS'})
+        dispatch({ type: 'LOADING_CLIENTS' })
         fetch(`${onlineUrl()}/clients`).then(res => {
             return res.json()
         })
-        .then(disClients => {
-            dispatch({type: 'ADD_CLIENTS', clients: disClients})
-        })
+            .then(disClients => {
+                dispatch({ type: 'ADD_CLIENTS', clients: disClients })
+            })
     }
+}
+
+export const createClient = client => {
+    return (dispatch) => {
+        fetch(`${onlineUrl()}/clients`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ client: client }) 
+            }).then(res => res.json())
+            .then(newClient => dispatch({type: "ADD_CLIENT", client: newClient}))
+        }
 }
