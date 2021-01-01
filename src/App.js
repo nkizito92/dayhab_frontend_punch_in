50b375a938e990e646374112897ec996e48083c7
nkizito92 <nkizito57@gmail.com>
2020-12-31 22:54:02 -0600

    create clientForm is added to the switch routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import ClockContainer from './container/ClockContainer'
 import ClientContainer from './container/ClientContainer'
+import ClientShow from './components/clientsComponents/ClientShow'
+import ClientForm from './components/clientsComponents/ClientForm'
 import Navbar from './headers/Navbar'
 import { fetchPunches } from './action/clockAction'
 import { fetchClients } from './action/clientAction'
@@ -18,6 +20,7 @@ class App extends React.Component {
  render () {
 
    console.log(this.props.punches)
+   console.log(this.props.clients)
    return (
 
      <div className="App">
@@ -27,6 +30,8 @@ class App extends React.Component {
      <Switch>
         <Route exact path="/" component={() => <ClockContainer punches={this.props.punches}/>} />
         <Route exact path="/clients" component={() => <ClientContainer clients={this.props.clients} />} />
+        <Route exact path= "/clients/new" component={() => <ClientForm /> } />
+        <Route exact path="/clients/:id" component={({match, history}) => <ClientShow clients={this.props.clients} history={history} match={Number(parseInt(match.params.id))}/>} />
      </Switch>
      </div>
   ) 
