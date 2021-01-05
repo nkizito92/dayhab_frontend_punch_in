2f256f3ae5d4ab1b79d0c3efadc23235cfce7962
nkizito92 <nkizito57@gmail.com>
2021-01-05 15:22:42 -0600

    flash with a list of Driver has been added in the form

--- src/components/clientsComponents/ClientForm.js
+++ src/components/clientsComponents/ClientForm.js
@@ -2,29 +2,38 @@ import React, { useState } from "react"
 import { createClient } from "../../action/clientAction"
 import { connect } from 'react-redux'
 
-const ClientForm = ({createClient}) => {
+const ClientForm = ({ createClient, drivers }) => {
     const [fullname, setfullName] = useState(0)
     const [pay_rate, setPay_rate] = useState(1)
-    const [driver_id, setDriver] = useState(2)
-
+    const [driverId, setDriver] = useState()
+    const [flash, setFlash] = useState()
+    let flashing = document.getElementById("flash")
+    let listOfDrivers = drivers.map(driver => {
+        return (
+            <option key={driver.id} id={"key" + driver.id}>{driver.first_name} {driver.last_name}</option>
+        )
+    })
     const handleSubmit = e => {
         e.preventDefault()
         let newClient = {
             full_name: fullname,
             pay_rate: Number(parseInt(pay_rate)),
-            driver_id: Number(parseInt(driver_id))
+            driver_id: Number(parseInt(driverId))
         }
+        flashing.className = "updated"
+        setFlash(`${fullname} is Created!!`)
 
         createClient(newClient)
     }
     return (
-        <div><h1>Client Form</h1> 
+        <div><h1>Client Form</h1>
             <form onSubmit={e => handleSubmit(e)}>
-                <input name="full_name" placeholder="fullname" onChange={e => setfullName(e.target.value)} /> <br/>
-                <input type="number" name="pay_rate" placeholder="pay_rate" onChange={e => setPay_rate(e.target.value)} /> <br/>
-                <input type="number" name="driver_id" placeholder="driver" onChange={e => setDriver(e.target.value)} /> <br/>
-                <input type="submit" onClick={ handleSubmit} value="Create Client"/>
+                <input name="full_name" placeholder="fullname" onChange={e => setfullName(e.target.value)} /> <br />
+                <input type="number" name="pay_rate" placeholder="pay_rate" onChange={e => setPay_rate(e.target.value)} /> <br />
+                <select name="driver_id" onChange={e => setDriver(e.currentTarget.selectedOptions[0].id.split("key")[1])}>{listOfDrivers}</select> <br />
+                <input type="submit" onClick={handleSubmit} value="Create Client" />
             </form>
+            <div id="flash">{flash}</div>
         </div>
     )
 }
