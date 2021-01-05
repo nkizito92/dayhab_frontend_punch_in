7acf55728a9f8813a12dab09f17d77e35776e831
nkizito92 <nkizito57@gmail.com>
2021-01-05 15:25:21 -0600

    drivers link is added to the navbar

--- src/headers/Navbar.js
+++ src/headers/Navbar.js
@@ -8,10 +8,11 @@ const link = {
     textDecoration: 'none'
 }
 
- const Navbar = () => (
+const Navbar = () => (
     <>
         <NavLink to="/" exact style={link} activeStyle={{ backgroundColor: "navy" }}>Home</NavLink>
         <NavLink to="/clients" exact style={link} activeStyle={{ backgroundColor: "navy" }}>Clients</NavLink>
+        <NavLink to="/drivers" exact style={link} activeStyle={{ backgroundColor: "navy" }}>Drivers</NavLink>
         <NavLink to="/clients/new" exact style={link} activeStyle={{ backgroundColor: "navy" }}>Create Client</NavLink>
     </>
 )
