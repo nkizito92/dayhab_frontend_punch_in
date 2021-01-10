81df32359579c2a8f3ca35fff9567b337f4cd5a2
nkizito92 <nkizito57@gmail.com>
2021-01-10 12:39:59 -0600

    driverContainer is added

---
+++ src/container/DriverContainer.js
@@ -0,0 +1,12 @@
+import Drivers from '../components/driversComponents/Drivers'
+
+const DriverContainer = ({drivers}) => {
+
+    return (
+        <div> 
+            <Drivers drivers={drivers}/>
+        </div>
+    )
+}
+
+export default DriverContainer
