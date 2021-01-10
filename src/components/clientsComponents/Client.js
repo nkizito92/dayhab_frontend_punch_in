161bf7672594e31e464f62626a43ad868b698e0e
nkizito92 <nkizito57@gmail.com>
2021-01-10 12:33:30 -0600

    fixed unique key, validation Dom error, and refacotry lines of code

--- src/components/clientsComponents/Client.js
+++ src/components/clientsComponents/Client.js
@@ -1,33 +1,47 @@
-const Client = ({ client }) => {
-    function setPayment(pay, hr) {
-        return pay * (hr / 5)
-    }
+import { Link } from "react-router-dom"
+const Client = ({ client, punches, month }) => {
     let time = []
-    let clockingOut = []
-    let clockingIn = []
-    let clockOuts = () => {
-        for (let i = 0; i < client.punches.length; i++) {
-            if (i % 2 !== 0) clockingOut.push(client.punches[i])
+    let theseClientPunches = punches.filter(punch => punch.client_id === client.id)
+    console.log("These Client punches", theseClientPunches)
+    let clockOuts = (clockingOut = []) => {
+        for (let i = 0; i < theseClientPunches.length; i++) {
+            if (i % 2 !== 0 && theseClientPunches[i].date === month) clockingOut.push(theseClientPunches[i])
         }
+        // for (let i of theseClientPunches) {
+        //     console.log(i)
+        //     if (i.id % 2 !== 0 && i.date === month) clockingOut.push(i)
+        // }
+        console.log("clockingOUts", clockingOut)
         const unique = [...new Map(clockingOut.map(cOut => [cOut.id, cOut])).values()]
         return unique
     }
 
-    let clockIns = () => {
-        for (let i = 0; i < client.punches.length; i++) {
-            if (i % 2 === 0) clockingIn.push(client.punches[i])
+    let clockIns = (clockingIn = []) => {
+        for (let i = 0; i < theseClientPunches.length; i++) {
+            if (i % 2 === 0 && theseClientPunches[i].date === month) clockingIn.push(theseClientPunches[i])
         }
+        // for (let i of theseClientPunches) {
+        //     if (i.id % 2 === 0 && i.date === month) clockingIn.push(i)
+        // }
         const unique = [...new Map(clockingIn.map(cIn => [cIn.id, cIn])).values()]
         return unique
     }
     let totalMinutes = () => {
         for (let i = 0; i < clockOuts().length; i++) {
-            time.push(((Number(parseInt(clockOuts()[i].clock_out))) - (Number(parseInt(clockIns()[i].clock_in)))))
+            time.push((
+                (Number(parseInt(clockOuts()[i].clock_out)))
+                - (Number(parseInt(clockIns()[i].clock_in)))
+            ))
         }
-        return time
+        const uniqueTime = [...new Map(time.map(cIn => [cIn.id, cIn])).values()]
+        return uniqueTime
+    }
+    // Some punch objects are missing
+    function setPayment(pay, hr) {
+        return pay * (hr / 5.00)
     }
     let time_convert = num => {
-        
+        console.log("number of minutes",num)
         function isWeekday(year, month, day) {
             let thisday = new Date(year, month, day).getDay();
             return thisday !== 0 && thisday !== 6;
@@ -35,8 +49,8 @@ const Client = ({ client }) => {
 
         function getWeekdaysInMonth(month, year) {
             let weekdays = 0;
-            for(var i=0; i< daysRightNow; i++) {
-                if (isWeekday(year, month, i+1)) weekdays++;
+            for (var i = 0; i < daysRightNow; i++) {
+                if (isWeekday(year, month, i + 1)) weekdays++;
             }
             return weekdays;
         }
@@ -62,25 +76,26 @@ const Client = ({ client }) => {
         // }
         // 5 hours a day is the clients pay rate
         // less then 5 a day is a percentage
-        if (allMinutes > 60 * 40) {
-            allMinutes = 60 * 40
-        }
+        // if (allMinutes > 60 * 40) {
+        //     allMinutes = 60 * 40
+        // }
         let hours = Math.floor(allMinutes / 60);
         let minutes = allMinutes % 60;
         // display time and the payment
+        // if month matches the time client clocked in display those clockins 
         return (
-                   setPayment(client.pay_rate, hours) 
-                   
-            )
-        
+            setPayment(client.pay_rate, hours)
+        )
+
     }
+
     return (
-        <tr>
-            <td>{client.full_name}</td>
+        <>
+            <td><Link to={`/clients/${client.id}`}>{client.full_name}</Link> </td>
             <td>${client.pay_rate}</td>
-            <td>{client.punches.map(punch => punch.clock_in)}</td>
-            <td>{"$"+time_convert(totalMinutes())}</td>
-        </tr>
+            <td>{totalMinutes()}</td>
+            <td>{"$" + time_convert(totalMinutes())}</td>
+        </>
     )
 }
 
