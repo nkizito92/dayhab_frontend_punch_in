4e49844775a980afa78f67f29a43fe09d7044473
nkizito92 <nkizito57@gmail.com>
2021-01-10 12:31:31 -0600

    imported the module axios

--- src/action/clockAction.js
+++ src/action/clockAction.js
@@ -1,5 +1,5 @@
 import { onlineUrl } from './urlLink'
-
+import axios from 'axios'
 export const fetchPunches = () => {
     return (dispatch) => {
         dispatch({type: 'LOADING_PUNCHES'})
@@ -11,4 +11,44 @@ export const fetchPunches = () => {
     }
 
 
+}
+
+export const addPunch = punched => {
+    return ({
+        type: "ADD_PUNCH",
+        punch: punched
+    })
+}
+
+export const createPunch = punched => {
+    return (dispatch) => {
+        fetch(`${onlineUrl()}/punches`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ punch: punched })
+        }
+
+        )
+            .then(res => res.json())
+            .then(punchIn => dispatch(addPunch(punchIn)))
+            .catch(error => {
+                console.log(error)
+            })
+    }
+}
+
+export const deletePunch = punch => {
+    return (dispatch) => {
+        axios.delete(`${onlineUrl()}/punches/${punch.id}`,{punch})
+        .then(res => dispatch({type: "DELETE_PUNCH", punch: res.data}))
+    }
+}
+
+export const updatePunch = punch => {
+    return (dispatch) => {
+        axios.patch(`${onlineUrl()}/punches/${punch.id}`,{punch})
+        .then(res => dispatch({type: "EDIT_PUNCH", punch: res.data}))
+    }
 }
