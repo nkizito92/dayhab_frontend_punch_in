9e09af9ca2d835b72ed8c63b63148ae4826ab859
nkizito92 <nkizito57@gmail.com>
2021-01-16 19:33:12 -0600

    added a link and a function that displays localtime

--- src/components/clockComponents/ClockEdit.js
+++ src/components/clockComponents/ClockEdit.js
@@ -1,20 +1,44 @@
 import { connect } from "react-redux"
-import React, {useState } from "react"
+import React, { useState } from "react"
 import { updatePunch, deletePunch } from "../../action/clockAction"
-const ClockEdit = ({ punches, history, match, updatePunch, deletePunch }) => {
+import { Link} from 'react-router-dom'
+const ClockEdit = ({ isLoggedIn, punches, history, match, updatePunch, deletePunch }) => {
     let punch = punches.find(punch => punch.id === match)
     const [clockIn, setClIn] = useState()
     const [clockOut, setClOut] = useState()
+    function displayLocalTime(mins) {
+        if (mins !== null) {
+            let localTime = 0
+            let hours = Number(parseInt(mins.split("").slice(0, 2).join("")))
+            let minutes = Number(parseInt(mins.split("").slice(2, 4).join("")))
+            if (minutes < 10) minutes = (`0${minutes}`)
+            if (hours > 12) {
+                localTime = (hours - 12) + ":" + minutes + " PM"
+            }
+            else { localTime = hours + ":" + minutes + " AM" }
+            return localTime
+        }
+    }
     let displayPunchForm = () => {
         if (punch !== undefined) {
-            return (
-                <form onSubmit={e => handleSubmit(e)}>
-                    <input name="clock_in" onChange={e => setClIn(e.target.value)} placeholder={punch.clock_in} /> <br />
-                    <input name="clock_out" onChange={e => setClOut(e.target.value)} placeholder={punch.clock_out} /> <br />
-                    <input type="submit" value="Upated Punch" onClick={handleSubmit} />
-                    <input type="submit" value="Delete Punch" onClick={e=>handleDelete(e)} />
-                </form>
-            )
+            let clockInHours = punch.clock_in
+            let clockOutHours = punch.clock_out
+            if (punch.clock_in) {
+                return (
+                <>
+                    <label>Clock In Time Was {displayLocalTime(clockInHours)}</label> <br />
+                    <input type="time" name="clock_in" onChange={e => setClIn(e.target.value)} /> <br />
+                </>
+                )
+            }
+            else {
+                return (
+                    <>
+                        <label>Clock Out Time Was {displayLocalTime(clockOutHours)}</label><br/>
+                        <input type="time" name="clock_out" onChange={e => setClOut(e.target.value)} placeholder={displayLocalTime(clockOutHours)} /> <br />
+                    </>
+                )
+            }
         } else {
             return <div className="loading"></div>
         }
@@ -22,31 +46,54 @@ const ClockEdit = ({ punches, history, match, updatePunch, deletePunch }) => {
 
     function handleSubmit(e) {
         e.preventDefault()
+        let convertLocalTime = ""
+        clockIn ? convertLocalTime = clockIn : convertLocalTime = clockOut
+        let hours = Number(parseInt(convertLocalTime.split(":")[0]))
+        let minutes = convertLocalTime.split(":")[1]
+        if(hours === 0 ) hours = 24
+        let timeItis = ""
+        timeItis = hours + minutes
         let punchUpdated = {
             id: match,
             clock_in: clockIn,
             clock_out: clockOut,
         }
+        if(clockIn) {
+            punchUpdated.clock_in = timeItis
+        } else {
+            punchUpdated.clock_out = timeItis
+        }
         updatePunch(punchUpdated)
-        setTimeout(() => history.push("/clients/"+punch.client_id), 2300)
+        setTimeout(() => history.push("/clients/" + punch.client_id), 2300)
     }
-
     function handleDelete(e) {
         e.preventDefault()
         let deletingPunch = {
             id: match,
-            clock_in: clockIn,
-            clock_out: clockOut
+            clock_in: punch.clock_in,
+            clock_out: punch.clock_out
         }
         deletePunch(deletingPunch)
-        setTimeout(() => history.push("/clients/"+punch.client_id), 2300)
+        setTimeout(() => history.push("/clients/" + punch.client_id), 2300)
+    }
+    let redirect = () => {
+        if (!isLoggedIn) {
+            <>{history.push("/login")}</>
+        } else {
+           return <Link to={`/clients`}>Go Back</Link>
+        }
     }
     return (
         <main>
-            {displayPunchForm()}
+            <h1>Edit Punch</h1>
+            <form onSubmit={e => handleSubmit(e)}>
+
+                {displayPunchForm()}
+                <input type="submit" value="Upated Punch" onClick={handleSubmit} />
+                <input type="submit" value="Delete Punch" onClick={e => handleDelete(e)} />
+            </form>
+            {redirect()}
         </main>
     )
-
 }
-
 export default connect(null, { updatePunch, deletePunch })(ClockEdit)
