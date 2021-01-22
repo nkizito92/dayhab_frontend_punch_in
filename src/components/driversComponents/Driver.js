c8973ea8e0636b6f45209eba2f5eeed5a3c76b7a
nkizito92 <nkizito57@gmail.com>
2021-01-21 22:45:25 -0600

    added the number of clients the driver has

--- src/components/driversComponents/Driver.js
+++ src/components/driversComponents/Driver.js
@@ -3,6 +3,7 @@ const Driver = ({driver}) => {
     return (
         <div>
             <h1>{driver.first_name} {driver.last_name}</h1>
+            <h2>Clients: {driver.clients.length}</h2>
             <Link to={"drivers/"+driver.id} >View Driver </Link>
         </div>
     )
