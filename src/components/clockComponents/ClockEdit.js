fd85a7e20596e4c65a141966249b4e4ed0767491
nkizito92 <nkizito57@gmail.com>
2021-01-25 07:43:31 -0600

    fixed time display

--- src/components/clockComponents/ClockEdit.js
+++ src/components/clockComponents/ClockEdit.js
@@ -1,7 +1,7 @@
 import { connect } from "react-redux"
 import React, { useState } from "react"
 import { updatePunch, deletePunch } from "../../action/clockAction"
-import { Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const ClockEdit = ({ isLoggedIn, punches, history, match, updatePunch, deletePunch, current_user }) => {
     let punch = punches.find(punch => punch.id === match)
     const [clockIn, setClIn] = useState()
@@ -12,10 +12,13 @@ const ClockEdit = ({ isLoggedIn, punches, history, match, updatePunch, deletePun
             let hours = Number(parseInt(mins.split("").slice(0, 2).join("")))
             let minutes = Number(parseInt(mins.split("").slice(2, 4).join("")))
             if (minutes < 10) minutes = (`0${minutes}`)
-            if (hours > 12) {
+            if (hours >= 12 && hours < 24) {
                 localTime = (hours - 12) + ":" + minutes + " PM"
             }
-            else { localTime = hours + ":" + minutes + " AM" }
+            else if(hours < 13 || hours === 24) {
+                if (hours === 24) hours = hours - 12
+                 localTime = hours + ":" + minutes + " AM" 
+                }
             return localTime
         }
     }
@@ -25,16 +28,16 @@ const ClockEdit = ({ isLoggedIn, punches, history, match, updatePunch, deletePun
             let clockOutHours = punch.clock_out
             if (punch.clock_in) {
                 return (
-                <>
-                    <label>Clock In Time Was {displayLocalTime(clockInHours)}</label> <br />
-                    <input type="time" name="clock_in" onChange={e => setClIn(e.target.value)} /> <br />
-                </>
+                    <>
+                        <label>Clock In Time Was {displayLocalTime(clockInHours)}</label> <br />
+                        <input type="time" name="clock_in" onChange={e => setClIn(e.target.value)} /> <br />
+                    </>
                 )
             }
             else {
                 return (
                     <>
-                        <label>Clock Out Time Was {displayLocalTime(clockOutHours)}</label><br/>
+                        <label>Clock Out Time Was {displayLocalTime(clockOutHours)}</label><br />
                         <input type="time" name="clock_out" onChange={e => setClOut(e.target.value)} placeholder={displayLocalTime(clockOutHours)} /> <br />
                     </>
                 )
@@ -50,7 +53,8 @@ const ClockEdit = ({ isLoggedIn, punches, history, match, updatePunch, deletePun
         clockIn ? convertLocalTime = clockIn : convertLocalTime = clockOut
         let hours = Number(parseInt(convertLocalTime.split(":")[0]))
         let minutes = convertLocalTime.split(":")[1]
-        if(hours === 0 ) hours = 24
+        if (hours === 0) hours = 24
+        if (hours < 10) hours = `0${hours}`
         let timeItis = ""
         timeItis = hours + minutes
         let punchUpdated = {
@@ -58,7 +62,7 @@ const ClockEdit = ({ isLoggedIn, punches, history, match, updatePunch, deletePun
             clock_in: clockIn,
             clock_out: clockOut,
         }
-        if(clockIn) {
+        if (clockIn) {
             punchUpdated.clock_in = timeItis
         } else {
             punchUpdated.clock_out = timeItis
@@ -73,15 +77,15 @@ const ClockEdit = ({ isLoggedIn, punches, history, match, updatePunch, deletePun
             clock_in: punch.clock_in,
             clock_out: punch.clock_out
         }
-        let clientsPunchs = punches.filter(punched => punched.client.full_name === punch.client.full_name)
-        deletePunch(clientsPunchs, deletingPunch)
+        let clientsPunches = punches.filter(punched => punched.client.full_name === punch.client.full_name)
+        deletePunch(clientsPunches, deletingPunch)
         setTimeout(() => history.push("/clients/" + punch.client_id), 2300)
     }
     let redirect = () => {
         if (!isLoggedIn) {
             <>{history.push("/login")}</>
         } else {
-           return <Link to={`/clients`}>Go Back</Link>
+            return <Link to={`/clients`}>Go Back</Link>
         }
     }
     return (
@@ -90,8 +94,8 @@ const ClockEdit = ({ isLoggedIn, punches, history, match, updatePunch, deletePun
             <form onSubmit={e => handleSubmit(e)}>
 
                 {displayPunchForm()}
-                <input type="submit" value="Upated Punch" onClick={handleSubmit} />
-                <input type="submit" value="Delete Punch" onClick={e => handleDelete(e)} />
+                <button onClick={handleSubmit} > Upated Punch</button>
+                <button onClick={e => handleDelete(e)} > Delete Punch</button>
             </form>
             {redirect()}
         </main>
