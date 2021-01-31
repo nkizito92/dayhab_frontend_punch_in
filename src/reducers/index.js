41ee7db9c2a1d40bd803f5e59135cddf63513e02
nkizito92 <nkizito57@gmail.com>
2021-01-30 20:36:22 -0600

    added a users reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import clockinsReducer from './clockinsReducer'
 import clientsReducer from './clientsReducer'
 import driversReducer from './driversReducer'
+import usersReducer from './usersReducer'
 
 export default combineReducers ({
     clockinsReducer,
     clientsReducer,
-    driversReducer
+    driversReducer,
+    usersReducer
 })
---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,19 @@
+const usersReducer = (state = { users: [], loading: false}, action) => {
+    switch(action.type){
+        case 'LOADING_USERS':
+            return {
+                ...state,
+                users: [...state.users],
+                loading: true
+            }
+        case 'ADD_USERS':
+            return {
+                ...state,
+                users: action.users
+            }
+
+        default: return state
+    }
+}
+
+export default usersReducer
