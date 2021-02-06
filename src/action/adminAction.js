67dbb52ce6b73de188f9c0d31ba78dbc7d05d439
nkizito92 <nkizito57@gmail.com>
2021-02-06 14:42:55 -0600

    changed histry to status message in the goLogin's params

--- src/action/adminAction.js
+++ src/action/adminAction.js
@@ -2,13 +2,18 @@ import axios from 'axios'
 import { onlineUrl } from './urlLink'
 
 // try adding {withCredentails: true}
-export const goLogin = (user, handleLogin, history) => {
+export const goLogin = (user, handleLogin, statusMessage) => {
     axios.post(`${onlineUrl()}/login`, { user }, { withCredentails: true }).then(res => {
         if (res.data.logged_in) {
             handleLogin(res.data)
+            statusMessage(res.data.message)
+        } else {
+            statusMessage(res.data.message)
         }
     })
 }
+
+
 
 export const goSignup = (user, handleLogin, history) => {
     axios.post(`${onlineUrl()}/users`, { user }, { withCredentails: true }).then(res => {
@@ -17,5 +22,12 @@ export const goSignup = (user, handleLogin, history) => {
             history.push("/clients")
         }
     })
+}
+
+export const fetchUsers = () => {
+    return (dispatch) => {
+        axios.get(`${onlineUrl()}/users`)
+        .then(res => dispatch({type: "ADD_USERS", users: res.data}))
+    }
 }
 
