17ab219bb568a0731529f4644217d4560444eb3f
nkizito92 <nkizito57@gmail.com>
2021-02-09 18:10:54 -0600

    added the clients props and matched them with their drivers

--- src/components/driversComponents/Driver.js
+++ src/components/driversComponents/Driver.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
-const Driver = ({driver}) => {
+const Driver = ({driver, clients}) => {
+    let driversClients = clients.filter(client => driver.id === client.driver_id)
     return (
         <div>
             <h1>{driver.first_name} {driver.last_name}</h1>
-            <h2>Clients: {driver.clients.length}</h2>
+            <h2>Clients: {driversClients.length}</h2>
             <Link to={"drivers/"+driver.id} >View Driver </Link>
         </div>
     )
