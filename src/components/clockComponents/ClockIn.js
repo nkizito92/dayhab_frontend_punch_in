bf8a6418e069c1749554ceabb99b3181ade7e62f
nkizito92 <nkizito57@gmail.com>
2021-02-11 17:29:47 -0600

    changed Tilte to Time Clock

--- src/components/clockComponents/ClockIn.js
+++ src/components/clockComponents/ClockIn.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { createPunch, updatePunch } from '../../action/clockAction'
+import Clock from './Clock'
 class ClockIn extends Component {
-
     state = {
         full_name: "",
         error: "",
-        update: ""
+        update: "",
     }
 
     handlePunch = e => {
@@ -20,55 +20,46 @@ class ClockIn extends Component {
             full_name: this.state.full_name,
         }
 
-        let flashUpdate = ""
-
         let checkFull_name = this.props.clients.find(client => client.full_name === this.state.full_name)
 
         if (checkFull_name !== undefined) {
-            document.getElementById("success").className = "updated"
-            let clients_punches = this.props.punchIn.filter(punch => this.state.full_name === punch.client.full_name).length + 1
-    
-            if (clients_punches % 2 === 0) {
-                flashUpdate = "Clock Out"
-                setTimeout(() => {
-                    this.props.history.push("/clients")
-                }, 3000);
-            } else {
-                flashUpdate = "Clock In"
-                
-            }
             this.setState({
                 client_id: "",
                 clock_in: "",
-                update: `${this.state.full_name} ${flashUpdate}`, // client clock in or out
                 full_name: ""
             })
-            this.props.createPunch(newPunch)
+            this.props.createPunch(newPunch, this.props.statusMessage)
         }
         else {
-            document.getElementById("fail").className = "error"
             this.setState({
-                error: "Client doesn't exist",
                 full_name: ""
             })
+            let message = {
+                status: "",
+                error: "Client Doesn't Exist!!"
+            }
+            this.props.statusMessage(message)
         }
     }
-     redirect ()  {
+    redirect() {
         if (!this.props.isLoggedIn) {
             <>{this.props.history.push("/login")}</>
-        } 
+        }
     }
-
+    time() {
+        let localTime = new Date().toLocaleTimeString([], { timeStyle: 'short' })
+        return localTime
+    }
     render() {
         return (
             <div>
+                <h1>Time Clock</h1>
+                <Clock />
+                <br />
                 <form onSubmit={e => this.handleSubmit(e)}>
-                    <h3>PunchMachine</h3>
-                    <input name="full_name" type="text" onChange={e => this.handlePunch(e)} value={this.state.full_name} />
-                    <input onClick={this.handleSubmit} type="submit" />
+                    <input name="full_name" type="text" onChange={e => this.handlePunch(e)} placeholder="Full Name" autoFocus /> <br />
+                    <button onClick={this.handleSubmit}>Submit</button>
                 </form>
-                <div id="success">{this.state.update}</div>
-                <div id="fail">{this.state.error}</div>
                 {this.redirect()}
             </div>
         )
