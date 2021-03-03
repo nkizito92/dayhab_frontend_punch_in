bba28f264cbb65c4e51e08f11a33373316ed93d0
nkizito92 <nkizito57@gmail.com>
2021-03-02 18:30:12 -0600

    added update action for User

--- src/action/adminAction.js
+++ src/action/adminAction.js
@@ -27,7 +27,22 @@ export const goSignup = (user, handleLogin, history) => {
 export const fetchUsers = () => {
     return (dispatch) => {
         axios.get(`${onlineUrl()}/users`)
-        .then(res => dispatch({type: "ADD_USERS", users: res.data}))
+            .then(res => dispatch({ type: "ADD_USERS", users: res.data }))
     }
 }
 
+export const updateUser = (statusMessage, editedUser) => {
+    return (dispatch) => {
+        debugger
+        axios.patch(`${onlineUrl()}/users/${editedUser.id}`, {editedUser})
+        .then(res => {
+            
+                if (res.data.message.success) {
+                    statusMessage(res.data.message)
+                    dispatch({ type: "EDIT_USER", user: res.data })
+                }
+                else statusMessage(res.data.message)
+            })
+            .catch(error => console.log(error))
+    }
+}
