66609e822548082fa25281710e7b0f5c8008c01e
nkizito92 <nkizito57@gmail.com>
2021-03-20 19:13:44 -0500

    refactoring the action

--- src/action/clientAction.js
+++ src/action/clientAction.js
@@ -7,30 +7,26 @@ export const addClient = client => {
     })
 }
 
-export const fetchClients = () => {
-    return (dispatch) => {
-        dispatch({ type: 'LOADING_CLIENTS' })
-        fetch(`${onlineUrl()}/clients`).then(res => {
-            return res.json()
+export const fetchClients = () => dispatch => {
+    dispatch({ type: 'LOADING_CLIENTS' })
+    fetch(`${onlineUrl()}/clients`).then(res => {
+        return res.json()
+    })
+        .then(disClients => {
+            dispatch({ type: 'ADD_CLIENTS', clients: disClients })
         })
-            .then(disClients => {
-                dispatch({ type: 'ADD_CLIENTS', clients: disClients })
-            })
-    }
 }
 
-export const createClient = client => {
-    return (dispatch) => {
-        fetch(`${onlineUrl()}/clients`,
-            {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ client: client })
-            }).then(res => res.json())
-            .then(newClient => dispatch({ type: "ADD_CLIENT", client: newClient }))
-    }
+export const createClient = client => dispatch => {
+    fetch(`${onlineUrl()}/clients`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ client: client })
+        }).then(res => res.json())
+        .then(newClient => dispatch({ type: "ADD_CLIENT", client: newClient }))
 }
 export const editClient = client => {
     return {
@@ -38,16 +34,12 @@ export const editClient = client => {
         client: client
     }
 }
-export const updateClient = client => {
-    return (dispatch) => {
-        axios.patch(`${onlineUrl()}/clients/${client.id}`, {client})
-            .then(res => dispatch(editClient(res.data)))
-    }
+export const updateClient = client => dispatch => {
+    axios.patch(`${onlineUrl()}/clients/${client.id}`, { client })
+        .then(res => dispatch(editClient(res.data)))
 }
 
-export const deleteClient = client => {
-    return (dispatch) => {
-        axios.delete(`${onlineUrl()}/clients/${client.id}`)
-        .then(res => dispatch({type: "DELETE_CLIENT", client: res.data}))
-    }
+export const deleteClient = client => dispatch => {
+    axios.delete(`${onlineUrl()}/clients/${client.id}`)
+        .then(res => dispatch({ type: "DELETE_CLIENT", client: res.data }))
 }
