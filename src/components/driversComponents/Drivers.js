b9d5e0bd169a6de96de78f9027350fa841b93f2e
nkizito92 <nkizito57@gmail.com>
2021-04-01 20:59:28 -0500

    link for create driver has been styled by css

--- src/components/driversComponents/Drivers.js
+++ src/components/driversComponents/Drivers.js
@@ -3,21 +3,27 @@ import { Link } from "react-router-dom"
 
 const Drivers = ({ drivers, isLoggedIn, current_user, clients }) => {
     let display = () => {
-        if(current_user){
-        let usersDrivers = drivers.filter(driver => driver.user_id === current_user) 
-        return usersDrivers.map(driver => {
-          return <Driver key={driver.id} driver={driver} clients={clients}/>
-        })} else {
-            return <div><Link to="/login">Login</Link> to see Drivers </div>
+        if (current_user) {
+            let usersDrivers = drivers.filter(driver => driver.user_id === current_user)
+            return usersDrivers.map(driver => {
+                return <Driver key={driver.id} driver={driver} clients={clients} />
+            })
+        } else {
+            return (
+                <><br/>
+                    <div><Link className="button" to="/login">Login To See Drivers</Link> </div>
+                </>)
         }
     }
     let displayCreateLink = () => {
-        if(isLoggedIn){
-        return (
-            <div>
-                <Link to="/drivers/new" >Create Driver</Link>
-            </div>
-        )}
+        if (isLoggedIn) {
+            return (
+                <div>
+                    <br />
+                    <Link className="button" to="/drivers/new" >Create Driver</Link>
+                </div>
+            )
+        }
     }
     return (
         <div>
