a1563871bac0b75338458522efb39c02d9f1ba79
nkizito92 <nkizito57@gmail.com>
2021-08-03 19:05:47 -0500

    create 2 methods to display Client list

--- src/components/clientsComponents/Clients.js
+++ src/components/clientsComponents/Clients.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Client from './Client'
+import ClientList from './ClientList'
 import { Link } from 'react-router-dom'
 class Clients extends Component {
     componentDidMount() {
@@ -8,7 +9,8 @@ class Clients extends Component {
         })
     }
     state = {
-        month: ""
+        month: "",
+        inputValue: ""
     }
 
     display(thesePunches) {
@@ -55,10 +57,45 @@ class Clients extends Component {
             )
         }
     }
+    goToClientpage(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+        let thisClient = this.props.clients.find(client => this.state.inputValue.toLowerCase() === client.full_name.toLowerCase())
+        if (thisClient) {
+            this.props.history.push(`/clients/${thisClient.id}`)
+        } else {
+            return
+        }
+    }
+
+    displayListOfClients(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+        let theseClients = this.props.clients.filter(client => this.state.inputValue.includes(client.full_name.slice(0, 3).toLowerCase()))
+        console.log(theseClients)
+    }
+
+    displayList() {
+        let theseClients = this.props.clients.filter(client => this.state.inputValue.includes(client.full_name.slice(0, 3).toLowerCase()))
+        if (theseClients !== undefined) {
+            return theseClients.map(client => {
+                return <div><ClientList key={`key-${client.id}`} client={client} /></div>
+            })
+        }
+    }
 
     render() {
         return (
             <div>
+                <form onSubmit={this.goToClientpage}>
+                    <div>
+                        <input onChange={(e) => this.displayListOfClients(e)} value={this.state.inputValue.toLowerCase()} type="search" placeholder="Search..." />
+                        <div className="clientsList">{this.displayList()}</div>
+                    </div>
+                    <input type="submit" onClick={(e) => this.goToClientpage(e)} value="Search Name" />
+                </form>
                 <h1>Clients</h1>
                 <select onChange={e => this.handleMonth(e)}>
                     <option hidden>{this.state.month}</option>
@@ -89,7 +126,7 @@ class Clients extends Component {
                     </tbody>
                 </table>
 
-                { this.displayLink()}
+                {this.displayLink()}
             </div >
         )
     }
