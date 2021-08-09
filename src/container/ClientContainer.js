4426df2dd625aac98c9a33de870cc18c0ced83b4
nkizito92 <nkizito57@gmail.com>
2021-08-08 21:45:46 -0500

    added history to props

--- src/container/ClientContainer.js
+++ src/container/ClientContainer.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Clients from '../components/clientsComponents/Clients'
 class ClientContainer extends React.Component {
-    render () {
-    return (
-        <div>
-            <Clients current_user={this.props.current_user} clients={this.props.clients} punches={this.props.punches} isLoggedIn={this.props.isLoggedIn}/>
-        </div>
-    )
-}
+    render() {
+        return (
+            <div>
+                <Clients history={this.props.history} current_user={this.props.current_user} clients={this.props.clients} punches={this.props.punches} isLoggedIn={this.props.isLoggedIn} />
+            </div>
+        )
+    }
 }
 
 export default ClientContainer
