53de18e5e145e76f174b7cdfa4797cd5e1baabd9
nkizito92 <nkizito57@gmail.com>
2021-08-11 22:35:14 -0500

    add a class name for the links

--- src/components/clientsComponents/ClientList.js
+++ src/components/clientsComponents/ClientList.js
@@ -3,8 +3,8 @@ const ClientList = ({ client }) => {
     let thisClient = 0
     if (client) thisClient = client
     return (
-        <div>
-            <Link to={`/clients/${thisClient.id}`}>{thisClient.full_name}</Link>
+        <div className="ListOfNames">
+            <Link className="clientNames" to={`/clients/${thisClient.id}`}>{thisClient.full_name}</Link>
         </div>
     )
 }
